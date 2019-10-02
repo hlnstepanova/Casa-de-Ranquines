ab1f6533572354acd2ca590ee0905009a5ce9f17
hlnstepanova_enveto <e.stepanova@enveto.de>
2019-10-02 13:39:45 -0300

    Change urls

--- src/services/statusService.js
+++ src/services/statusService.js
@@ -1,6 +1,5 @@
 import http from "./httpService";
-import { apiUrl } from "../config.json";
 
 export function getStatuses() {
-  return http.get(apiUrl + "/statuses");
+  return http.get("/statuses");
 }
